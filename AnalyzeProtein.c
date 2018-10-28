d8839c705a87f9a8bec2b15a34e106071175713d
elkanatovey <40407298+elkanatovey@users.noreply.github.com>
2018-10-28 11:54:08 +0200

    Initial commit

---
+++ AnalyzeProtein.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int receiveInput()
+{
+//open file if doesnt exist error + message
+}
+
+
+/**
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char *argv[]) {
+    int i = 1;
+    for(i; i<argc; i++)
+    {
+        //check input legal
+        if//analyze
+        else//break and receive new args
+    }
+    return 0;
+}
